4c5e85fb39862b37581f7b9e330fbd4e377f6b93
Vitaliy Andryushkov <viandryushkov@avito.ru>
2019-11-22 18:50:07 +0300

    fix prod build

--- next.config.js
+++ next.config.js
@@ -1,81 +1,68 @@
 const cssLoaderConfig = require('@zeit/next-css/css-loader-config');
-const lessToJS = require('less-vars-to-js');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const withLess = require('@zeit/next-less');
+const lessToJS = require('less-vars-to-js');
 const fs = require('fs');
 const path = require('path');
 
-module.exports = (nextConfig = {}) => {
-  const themeVariables = lessToJS(
-    fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
-  );
+const themeVariables = lessToJS(
+  fs.readFileSync(path.resolve(__dirname, './assets/antd-custom.less'), 'utf8')
+);
 
-  return Object.assign({}, nextConfig, {
-    webpack (config, options) {
-      // fix: prevents error when .less files are required by node
-      if (typeof require !== 'undefined') {
-        require.extensions['.less'] = file => {}; // eslint-disable-line node/no-deprecated-api
-      }
-
-      const {
-        cssModules,
-        cssLoaderOptions,
-        postcssLoaderOptions
-      } = nextConfig;
-
-      const { dev, isServer } = options;
-
-      options.defaultLoaders.less = cssLoaderConfig(config, {
-        extensions: ['less'],
-        cssModules,
-        cssLoaderOptions,
-        postcssLoaderOptions,
-        dev,
-        isServer,
-        loaders: [
-          {
-            loader: 'less-loader',
-            options: {
-              javascriptEnabled: true,
-              modifyVars: themeVariables
-            }
-          },
-        ]
-      });
-
-      config.module.rules.push({
-        test: /\.less$/,
-        use: options.defaultLoaders.less
-      });
-
-      options.defaultLoaders.css = cssLoaderConfig(config, {
-        extensions: ['css'],
-        cssModules: true,
-        cssLoaderOptions: {
-          localIdentName: '_[local]--[hash:base64:5]'
+module.exports = withLess({
+  lessLoaderOptions: {
+    javascriptEnabled: true,
+    modifyVars: themeVariables, // make your antd custom effective
+  },
+  webpack: (config, { isServer, dev, defaultLoaders }) => {
+    if (isServer) {
+      const antStyles = /antd\/.*?\/style.*?/;
+      const origExternals = [...config.externals];
+      config.externals = [
+        (context, request, callback) => {
+          if (request.match(antStyles)) return callback();
+          if (typeof origExternals[0] === 'function') {
+            origExternals[0](context, request, callback);
+          } else {
+            callback();
+          }
         },
-        postcssLoaderOptions,
-        dev,
-        isServer
+        ...(typeof origExternals[0] === 'function' ? [] : origExternals),
+      ];
+
+      config.module.rules.unshift({
+        test: antStyles,
+        use: 'null-loader',
       });
-
-      config.module.rules.push({
-        test: /\.css$/,
-        include: /\.module\.css$/,
-        use: options.defaultLoaders.css
-      });
-
-      if (process.env.ANALYZE) {
-        config.plugins.push(
-          new BundleAnalyzerPlugin({
-            analyzerMode: 'static',
-            reportFilename: options.isServer
-              ? '../analyze/server.html'
-              : './analyze/client.html',
-          })
-        );
-      }
-
-      return config;
     }
-  });
-};
+
+    defaultLoaders.css = cssLoaderConfig(config, {
+      extensions: ['css'],
+      cssModules: true,
+      cssLoaderOptions: {
+        localIdentName: '_[local]--[hash:base64:5]'
+      },
+      dev,
+      isServer
+    });
+
+    config.module.rules.push({
+      test: /\.css$/,
+      include: /\.module\.css$/,
+      use: defaultLoaders.css
+    });
+
+    if (process.env.ANALYZE) {
+      config.plugins.push(
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static',
+          reportFilename: isServer
+            ? '../analyze/server.html'
+            : './analyze/client.html',
+        })
+      );
+    }
+
+    return config;
+  },
+});
