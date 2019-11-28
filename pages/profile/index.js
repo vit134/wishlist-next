a69d8d0e4f9ca19cc7f56d8b7476f6d7b03aad19
Vitaliy Andryushkov <viandryushkov@avito.ru>
2019-11-29 02:02:49 +0300

    remove bootstrap from profile page

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -2,8 +2,10 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { wishesRequest } from '../../src/requests';
 import Link from 'next/link';
-import { Table } from 'react-bootstrap';
+import { Table } from 'antd';
 import { Image } from '../../src/components/image';
+
+const { Column } = Table;
 
 class ProfilePage extends React.Component {
   static async getInitialProps ({ req, res, ...props }) {
@@ -22,43 +24,31 @@ class ProfilePage extends React.Component {
   }
 
   render () {
-    console.log(this.props);
     const { wishes = {}, user = {} } = this.props;
     const { data = [] } = wishes;
 
     const { data: userData = {} } = user;
 
     return (
-      <Table striped bordered hover>
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Название</th>
-            <th>Цена</th>
-            <th>Бронь</th>
-          </tr>
-        </thead>
-        <tbody>
-          {
-            data.map((el, ind) => {
-              return (
-                <tr key={el._id}>
-                  <td>{ind + 1}</td>
-                  <td>
-                    {el.image && (
-                      <span style={{ display: 'inline-block', maxWidth: '40px', marginRight: '10px' }}>
-                        <Image src={el.image} size={50} crop/>
-                      </span>
-                    )}
-                    <Link href={`/user/${userData.username}/${el._id}`}>{ el.name }</Link>
-                  </td>
-                  <td>{ el.price }</td>
-                  <td>{ el.assigned && el.assigned }</td>
-                </tr>
-              );
-            })
-          }
-        </tbody>
+      <Table dataSource={data}>
+        <Column
+          title="Название"
+          dataIndex="name"
+          key="name"
+          render={(text, el) => {
+            return (
+              <>
+                {el.image && (
+                  <span style={{ display: 'inline-block', maxWidth: '40px', marginRight: '10px' }}>
+                    <Image src={el.image} size={50} crop/>
+                  </span>
+                )}
+                <Link href={`/user/${userData.username}/${el._id}`}>{ el.name }</Link>
+              </>
+            );
+          }}
+        />
+        <Column title="Цена" dataIndex="price" key="price" />
       </Table>
     );
   }
